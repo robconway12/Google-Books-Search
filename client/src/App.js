99794c77abf79da2483efc0d1cd2fe593651806a
Rob Conway <robconway12@gmail.com>
2019-07-21 14:58:16 -0700

    worked on controllers and models

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
+import Books from "./pages/Books";
+import SavedBooks from "./pages/SavedBooks";
+import NoMatch from "./pages/NoMatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/savedbooks" component={SavedBooks} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
